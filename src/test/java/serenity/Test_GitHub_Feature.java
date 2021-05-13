8374f571f8ed66d1ecc727a492ca3ad80a206e69
Noorl <ab427902@dal.ca>
2021-05-13 12:30:33 -0400

    Serenity practice

---
+++ src/test/java/serenity/Test_GitHub_Feature.java
@@ -0,0 +1,36 @@
+package serenity;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+
+import io.restassured.http.ContentType;
+import static org.hamcrest.core.Is.is;
+import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@SerenityTest
+public class Test_GitHub_Feature {
+
+    @DisplayName("This is for GitHub Test1")
+    @Test
+    @Tag("tag1")
+    public void testGitHubGetOneUserEndpoitTest(){
+       given()
+                .contentType(ContentType.JSON)
+                .pathParam("username","CybertekSchool")
+        .when()
+                .get("https://api.github.com/users/{username}")
+         .then()
+                .assertThat()
+                .statusCode(200)
+               // .log().all();
+        ;
+        Ensure.that("Test my GitHub Return the data1",
+                response -> response.body("login",is("CybertekSchool")));
+
+
+    }
+
+}
